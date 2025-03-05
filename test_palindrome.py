0c683ef64fa2c9962b4c6884566305868d7da1f7
Zachary Tome <zackktome@gmail.com>
2025-03-05 18:20:35 -0500

    Update test_palindrome.py

--- test_palindrome.py
+++ test_palindrome.py
@@ -6,3 +6,6 @@ class PalindromeTest(unittest.TestCase):
       def test_non_string(self):
         with self.assertRaises(ValueError):
             is_palindrome(12345)
+
+      def test_empty_string(self):
+        self.assertFalse(is_palindrome(""))
