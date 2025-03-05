4667ac34054577de686d69f719e942ebc8e6774d
Zachary Tome <zackktome@gmail.com>
2025-03-05 18:13:49 -0500

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -6,3 +6,15 @@ def is_palindrome(value: str) -> bool:
 
     if value == "":
         return False
+
+    value = ''.join(char.lower() for char in value if char.isalnum())
+
+    p = deque(value)
+
+    while len(p) > 1:
+        if p.popleft() != p.pop():
+            print(False)
+            return False
+
+    print(True)
+    return True
