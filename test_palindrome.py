30a2824148db4c63ba44a6a9dd92779b51fcb64e
Zachary Tome <zackktome@gmail.com>
2025-03-05 18:18:21 -0500

    Update test_palindrome.py

--- test_palindrome.py
+++ test_palindrome.py
@@ -2,3 +2,7 @@ import unittest
 from palindrome import is_palindrome
 
 class PalindromeTest(unittest.TestCase):
+
+      def test_non_string(self):
+        with self.assertRaises(ValueError):
+            is_palindrome(12345)
