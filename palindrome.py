ed3cbe99b6298523e28f58aea70eb57d4cabdc4e
Zachary Tome <zackktome@gmail.com>
2025-03-05 17:45:49 -0500

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,8 @@
-"""
-Validates strings as palindromes.
-"""
+from collections import deque
+
+def is_palindrome(value: str) -> bool:
+    if not isinstance(value, str):
+        return False
+
+    if value == "":
+        return False
